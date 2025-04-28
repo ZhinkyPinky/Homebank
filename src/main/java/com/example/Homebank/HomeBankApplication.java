743da7e0dfbe80099843ae500d2680e5c7ed0de1
Zhinky <jonathan.engstrom@gmail.com>
2025-04-28 11:20:57 +0200

    +Dockerfile.

--- src/main/java/com/example/Homebank/HomeBankApplication.java
+++ src/main/java/com/example/Homebank/HomeBankApplication.java
@@ -14,23 +14,6 @@ public class HomeBankApplication {
 
     public static void main(String[] args) {
         configurableApplicationContext = SpringApplication.run(HomeBankApplication.class, args);
-
-        /*
-        UserRepository userRepository = configurableApplicationContext.getBean(UserRepository.class);
-        List<UserEntity> users = userRepository.findAll();
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        users.forEach((user -> {
-            user.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(user);
-        }));
-         */
-
-
-//        byte[] secret = new byte[64];
-//        SecureRandom secureRandom = new SecureRandom();
-//        secureRandom.nextBytes(secret);
-//        String base64Secret = Base64.getEncoder().encodeToString(secret);
-//        System.out.println(base64Secret);
     }
 
 }
