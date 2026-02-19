12c34dffb1d20eeb9df12f5a3a0cc29b8296da39
Zhinky <jonathan.engstrom@gmail.com>
2026-02-19 20:46:23 +0100

    Revert password encoder to BCrypt

--- src/main/java/com/example/Homebank/businessLogic/security/SecurityConfig.java
+++ src/main/java/com/example/Homebank/businessLogic/security/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -81,6 +81,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
+        return new BCryptPasswordEncoder();
     }
 }
